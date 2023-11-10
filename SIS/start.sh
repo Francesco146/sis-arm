fc7647c58d17dcc5ea7c7ab9f238ec5bd2153028
Francesco Marastoni <marastoni14@gmail.com>
2023-11-10 12:23:16 +0100

    Add container_exists function to have some persistency

--- SIS/start.sh
+++ SIS/start.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+function container_exists() {
+    docker inspect -f '{{.State.Running}}' sis-arm 2>/dev/null || echo "false"
+}
+
 if [ "$(basename "$PWD")" != "SIS" ]; then
     echo "Assicurati di essere nella cartella 'SIS' per eseguire lo script. Ovvero la cartella che conterrà i file BLIF e il file .bsis_history"
 else
     docker build -t sis-arm ..
-    docker run -it -v "$PWD/.:/home/dockeruser/SIS" -v "$PWD/.bsis_history:/home/dockeruser/.bsis_history" sis-arm /bin/bash
+    if [ "$(container_exists)" != "false" ]; then
+        docker run -it --name sis-arm -v "$PWD/.:/home/dockeruser/SIS" -v "$PWD/.bsis_history:/home/dockeruser/.bsis_history" sis-arm /bin/bash
+    else
+        docker start -i sis-arm
+    fi
 fi
+
